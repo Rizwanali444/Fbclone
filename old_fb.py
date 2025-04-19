4c1582e917c5a22868b1cc54280035f7c3540afa
Rizwan Ali <120648247+Rizwanali444@users.noreply.github.com>
2025-04-19 09:18:29 +0500

    Update old_fb.py

--- old_fb.py
+++ old_fb.py
@@ -1,1 +1,10 @@
+import logging
 import coconuts
+
+logging.basicConfig(level=logging.DEBUG)
+
+try:
+    coconuts.main()
+except Exception as e:
+    logging.error("An error occurred", exc_info=True)
+
